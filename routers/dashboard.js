0c85f7de5e4dab69994376c1caf0742ab9f11dc8
DuckeyDev <discordtutorialsworld@gmail.com>
2024-06-14 16:11:46 +0000

    Codespaces Corrupted

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,34 +1,35 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.render('../views/dashboard/index.ejs', {
-        title: process.env.title,
-        req: req,
-        res: res,
-        role: req.session.role,
-        name: req.session.username
-    })
-})
+router.get("/", async (req, res) => {
+  res.render("../views/dashboard/index.ejs", {
+    title: process.env.title,
+    req: req,
+    res: res,
+    role: req.session.role,
+    name: req.session.username,
+  });
+});
 
-router.get('/images', async (req, res) => {
-    res.render('../views/dashboard/images.ejs', {
-        title: process.env.title,
-        req: req,
-        res: res,
-        role: req.session.role,
-        name: req.session.username,
-        pass: req.session.not_listd
-    })
-})
+router.get("/images", async (req, res) => {
+  res.render("../views/dashboard/images.ejs", {
+    title: process.env.title,
+    req: req,
+    res: res,
+    role: req.session.role,
+    name: req.session.username,
+    pass: req.session.not_listd,
+  });
+});
 
-router.get('/texts', async (req, res) => {
-    res.render('../views/dashboard/files.ejs', {
-        title: process.env.title,
-        req: req,
-        res: res,
-        role: req.session.role,
-        name: req.session.username
-    })
-})
-module.exports = router
+router.get("/texts", async (req, res) => {
+  res.render("../views/dashboard/files.ejs", {
+    title: process.env.title,
+    req: req,
+    res: res,
+    role: req.session.role,
+    name: req.session.username,
+    pass: req.session.not_listd,
+  });
+});
+module.exports = router;
